b3339a62dfea6e25702270ef958a8d869e6731a9
enzo78510 <122868396+enzo78510@users.noreply.github.com>
2024-11-12 15:52:40 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,30 +4,29 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
+	if (argc == 4 || ( argc == 3 && strcmp(argv[1], "sq") ==0))}
 	{
 	char* op = argv[1];
 	char* a = argv[2];
-	char* b = argv[3];
 	double r = 0;
-	if (strcmp(op, "add") == 0) {
+	if (strcmp(op, "add")== 0 && argc == 4) {
 		r = _add(atof(a), atof(b));
 		printf("%lf",r);
 	}
-	else if (strcmp(op, "sub") == 0) {
+	else if (strcmp(op, "sub") == 0 && argc == 4) {
 		r = _sub(atof(a), atof(b));
 		printf("%lf",r);
 	}
-	else if (strcmp(op, "mul") == 0) {
+	else if (strcmp(op, "mul") == 0 && argc == 4){
 		r = _mul(atof(a), atof(b));
 		printf("%lf",r);
 	}
-	else if (strcmp(op, "div") == 0) {
+	else if (strcmp(op, "div") == 0 && argc == 4) {
 		r = _div(atof(a), atof(b));
 		printf("%lf",r);
 	}
 
-	else if (strcmp(op, "sq") == 0) {
+	else if (strcmp(op, "sq")== 0 && argc == 3) {
 		r = _sq(atof(a));
 		printf("%lf",r);
 	}
